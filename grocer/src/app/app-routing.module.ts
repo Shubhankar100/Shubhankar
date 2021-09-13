2a3a2f1f25d8ff9ebdc243ff843e88eae118f6c1
Matthew Cumbo <mattcumbo99@gmail.com>
2021-09-13 14:21:31 -0500

    User login redirection

--- grocer/src/app/app-routing.module.ts
+++ grocer/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserPanelComponent } from './user-panel/user-panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"userLogin",component:UserPanelComponent},
+  {path:"",redirectTo:"",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
