bf564dd2e1ba7ff85b38fa4ff8276ea243082e5e
Matthew Cumbo <mattcumbo99@gmail.com>
2021-09-13 15:32:47 -0500

    Form functionality

--- grocer/src/app/register-normal-user/register-normal-user.component.ts
+++ grocer/src/app/register-normal-user/register-normal-user.component.ts
@@ -7,14 +7,32 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./register-normal-user.component.css']
 })
 export class RegisterNormalUserComponent implements OnInit {
+  // Message for form validation
+  registrationMessage:string = "";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  registerUser(registerRef:NgForm) {
+  // Gets a random number from 100,000 to 999,999
+  genRandomId(): number {
+    return (Math.random() * (999999 - 100000) + 100000);
+  }
+
+  // Called when the user hits the submit button
+  registerUser(registerRef:NgForm): void {
     let registerForm = registerRef.value;
+
+    // Make sure the passwords match
+    let pass1 = registerForm.password;
+    let pass2 = registerForm.repassword;
+    if (pass1 != pass2) {
+      this.registrationMessage = "Error: Passwords do not match";
+      return;
+    }
+
+    // Grab the user input from the fields
   }
 
 }
