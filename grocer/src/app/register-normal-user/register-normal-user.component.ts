adeca9f047a09cc9a8b2dd246fba4ecec27d05ec
Matthew Cumbo <mattcumbo99@gmail.com>
2021-09-13 14:55:02 -0500

    Initial register user commit

---
+++ grocer/src/app/register-normal-user/register-normal-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-normal-user',
+  templateUrl: './register-normal-user.component.html',
+  styleUrls: ['./register-normal-user.component.css']
+})
+export class RegisterNormalUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
