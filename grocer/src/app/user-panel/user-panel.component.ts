7536280c4aa496ec49a345af231f1b089376c93e
NoahCalabro <noahdcalabro@mgail.com>
2021-09-14 16:26:43 -0500

    Noah's User stuff is completed...minus the whole Mongo thing

--- grocer/src/app/user-panel/user-panel.component.ts
+++ grocer/src/app/user-panel/user-panel.component.ts
@@ -14,15 +14,12 @@ export class UserPanelComponent implements OnInit {
   fundsFlag:boolean=false;
 
   orderTable:string="";
+  editResponse:string="";
 
+  currentFunds:string="$"
+  money:number = 5; //CHANGE THIS LATER WHEN WE CAN GET THE ACTUAL VALUE
 
-  constructor(public router:Router) { }
-
-  ngOnInit(): void {
-  }
-
-
-  orderStatus(){
+  orderStatus(): void {
     let tableHead = "<table><tr><th>Order ID</th><th>Order Status</th></tr>";
     let tableRowStart = "<tr><td>";
     let tableRowMid = "</td><td>";
@@ -39,12 +36,61 @@ export class UserPanelComponent implements OnInit {
     //Order Id may be replaced by whatever method we use to recognize orders
 
     // for (let order of Orders){
-    //   let newRow = tableRowStart+order.id+tableRowMid+order.status+tableRowEnd;
+    //   let newRow = tableRowStart+order._userId+tableRowMid+order.status+tableRowEnd;
 
     //   tableContents+=newRow;
     // }
 
     this.orderTable=tableContents+tableEnd;
+  }
+
+  updateCurrentFunds(): void{
+    //get the funds currently on the profile.
+
+    //By default, let's assume it's five bucks
+    this.currentFunds="$"+this.money.toString();
+  }
+
+  constructor(public router:Router) { }
+
+  ngOnInit(): void {
+    this.orderStatus();
+    this.updateCurrentFunds();
+  }
+
+
+  checkIfEmpty(newProfileStuff:any): boolean {
+    if(newProfileStuff.password=="" && newProfileStuff.address=="" && newProfileStuff.email=="" && newProfileStuff.phone==""){
+      return true;
+    }
+    else if(newProfileStuff.password==null && newProfileStuff.address==null && newProfileStuff.email==null && newProfileStuff.phone==null){
+      return true;
+    }
+    else return false;
+  }
+
+  editProfile(editUserRef:NgForm): void {
+    let newProfileValue = editUserRef.value;
+    editUserRef.resetForm();
+    console.log(typeof newProfileValue);
+
+    if(newProfileValue.password!=newProfileValue.repassword){
+      this.editResponse="Passwords do not match!"
+    }
+    else if (this.checkIfEmpty(newProfileValue)){
+      this.editResponse="No fields filled!"
+    }
+    else{
+      //Do stuff with info.
+
+      this.editResponse="Profile updated!"
+    }
+  }
+
+  updateFunds(additionalFundsForm:NgForm):void {
+    let additionalFunds = additionalFundsForm.value.funds;
+    this.money += +additionalFunds;
+    this.updateCurrentFunds();
   }
 
 }
